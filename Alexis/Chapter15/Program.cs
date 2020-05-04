25a0ff3434ce9fd85b2e8e05f9bb6ddb51cecac6
alexis.d.horton03@gmail.com <alexis.d.horton03@gmail.com>
2020-05-04 17:01:09 -0500

    -gave a list of numbers in array form -printed out the numbers -reversed the numbers in arraay form

--- Alexis/Chapter15/Program.cs
+++ Alexis/Chapter15/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Chapter15
 {
@@ -18,6 +20,44 @@ namespace Chapter15
     {
         static void Main(string[] args)
         {
+            var gNumbers = GenerateNumbers(3);
+            Reverse(gNumbers);
+            PrintNumbers(gNumbers);
+            
+            Console.ReadLine();
+        }
+
+        static int[] GenerateNumbers(byte num)
+        {
+            List<int> retArray2 = new List<int>() 
+            {
+                45, 87, 6, 96, 2, 62, 30, 48, 7, 1
+            };
+
+            return retArray2.GetRange(0, num).ToArray();
+
+
+        }
+
+        static void PrintNumbers(int[] nums)
+        {
+            string prtStr = string.Empty;
+            foreach (var num in nums)
+            {
+                prtStr += num.ToString() + ", ";
+            }
+            Console.WriteLine(@"Numbers from Array: " + prtStr.Remove(prtStr.Length - 2, 2));
+
+        }
+
+        static void Reverse(int[] nums)
+        {
+            string prtStr = string.Empty;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                prtStr += nums[i].ToString() + ", ";
+            }
+            Console.WriteLine(@"Reversed numbers in array: " + prtStr.Remove(prtStr.Length - 2, 2));
         }
     }
 }
