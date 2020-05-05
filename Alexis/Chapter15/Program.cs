02221a96d2826226b41d47a5422932418f1a6ebf
alexis.d.horton03@gmail.com <alexis.d.horton03@gmail.com>
2020-05-05 17:03:22 -0500

    -Imputed in an enum for Fibonacci -Started on switch statement

--- Alexis/Chapter15/Program.cs
+++ Alexis/Chapter15/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 
 namespace Chapter15
 {
@@ -18,15 +19,22 @@ namespace Chapter15
     /// </summary>
     class Program
     {
+        enum fibSeq 
+        { fib1 = 1, fib2 = 1, fib3 = 2, fib4 = 3, fib5 = 5, 
+          fib6 = 8, fib7 = 13, fib8 = 21, fib9 = 34, fib10 = 55};
         static void Main(string[] args)
         {
+            /*
             var gNumbers = GenerateNumbers(3);
             Reverse(gNumbers);
             PrintNumbers(gNumbers);
             
             Console.ReadLine();
-        }
+            */
 
+
+        }
+        /*
         static int[] GenerateNumbers(byte num)
         {
             List<int> retArray2 = new List<int>() 
@@ -59,5 +67,48 @@ namespace Chapter15
             }
             Console.WriteLine(@"Reversed numbers in array: " + prtStr.Remove(prtStr.Length - 2, 2));
         }
+        */
+
+        static void Fibonacci()
+        {
+            switch (fibSeq)
+            {
+                case 1:
+                    break;
+                case fib2:
+                    break;
+                case fibSeq.fib3:
+                    break;
+                case fibSeq.fib4:
+                    break;
+                case fibSeq.fib5:
+                    break;
+                case fibSeq.fib6:
+                    break;
+                case fibSeq.fib7:
+                    break;
+                case fibSeq.fib8:
+                    break;
+                case fibSeq.fib9:
+                    break;
+                case fibSeq.fib10:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
+
+
 }
