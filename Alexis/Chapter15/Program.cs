cd8bcca6ed8067066991afe62ea734c314715f25
alexis.d.horton03@gmail.com <alexis.d.horton03@gmail.com>
2020-05-06 21:29:28 -0500

    -Struggled for hours until today -Switched postitions of the foreach statement -Made it pretty to look at -Created Fibonacci method

--- Alexis/Chapter15/Program.cs
+++ Alexis/Chapter15/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace Chapter15
 {
@@ -20,16 +21,34 @@ namespace Chapter15
     {
         static void Main(string[] args)
         {
-            var gNumbers = GenerateNumbers(3);
-            Reverse(gNumbers);
-            PrintNumbers(gNumbers);
-            
+
+            //var gNumbers = GenerateNumbers(3);
+            //Reverse(gNumbers);
+            //PrintNumbers(gNumbers);
+            int[] retFibon = new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+            int pretty3 = 0;
+            int pretty2 = 0;
+            int Pretty = 1;
+
+            foreach (var fibs in retFibon)
+            {
+                if (fibs > 0)
+                {
+                    Console.WriteLine("Fibonacci(" + Pretty++ + ") = " + fibs);
+                }
+
+            }
+            pretty3 = 3;
+            pretty2 = Fibonacci(pretty3);
+            Console.WriteLine("You chose Fibonacci(" + pretty3 + ") and that equals: " + pretty2);
+
             Console.ReadLine();
+
         }
 
         static int[] GenerateNumbers(byte num)
         {
-            List<int> retArray2 = new List<int>() 
+            List<int> retArray2 = new List<int>()
             {
                 45, 87, 6, 96, 2, 62, 30, 48, 7, 1
             };
@@ -59,5 +78,14 @@ namespace Chapter15
             }
             Console.WriteLine(@"Reversed numbers in array: " + prtStr.Remove(prtStr.Length - 2, 2));
         }
+        static int Fibonacci(int num)
+        {
+            int[] retFibon = new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
+
+
+            return retFibon[num];
+        }
+
+
     }
 }
